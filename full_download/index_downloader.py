b42d3da9278cf34e15deb61fe9ec7d21cd84e044
Sreejith S <sreejith2904@gmail.com>
2017-12-17 18:51:34 -0500

    update with log statement

---
+++ full_download/index_downloader.py
@@ -0,0 +1,9 @@
+
+
+__author__ = "Sreejith Sreekumar"
+__email__ = "sreekumar.s@husky.neu.edu"
+__version__ = "0.0.1"
+
+
+base_url = "https://www.sec.gov/Archives/edgar/full-index/"
+years = list(range(1993, 2018))
